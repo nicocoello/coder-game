17761ed65172368ef0775d9ea2def576188da748
nicocoello <coellonicolas97@gmail.com>
2022-02-08 13:04:25 -0300

    New Box Respawn & Camera, Animation Fixes

--- CoderGame/Assets/Scripts/TriggerEvents/DissapearBox.cs
+++ CoderGame/Assets/Scripts/TriggerEvents/DissapearBox.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class DissapearBox : MonoBehaviour
 {
-    public float time;    
+    
+    [SerializeField] private GameObject _box;    
+    public float timeToDestroy;
+    private float _timeToRespawn = 10f;    
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Invoke("Destroy", time);            
+            Invoke("Destroy", timeToDestroy);
+            Invoke("BoxRespawn", _timeToRespawn);
         }
-    }    
+    }
     public void Destroy()
     {
-        Destroy(gameObject);       
+        _box.SetActive(false);     
+    }
+    public void BoxRespawn()
+    {
+        _box.SetActive(true);        
     }
 }
